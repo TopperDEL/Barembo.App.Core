791975eababa6867e1affc179c01d842e2a25c20
TopperDEL <github_com@tparth.de>
2020-10-27 09:47:24 +0100

    doc: Enhance documentation

--- Barembo.App.Core/Interfaces/ILoginService.cs
+++ Barembo.App.Core/Interfaces/ILoginService.cs
@@ -7,10 +7,27 @@ using System.Threading.Tasks;
 
 namespace Barembo.App.Core.Interfaces
 {
+    /// <summary>
+    /// A LoginService is responsible for Login and Logout of a user via a StoreAccess.
+    /// </summary>
     public interface ILoginService
     {
+        /// <summary>
+        /// Returns if the current user is logged in
+        /// </summary>
+        /// <returns>True, if the user is logged in; false if not</returns>
         bool GetIsLoggedIn();
+
+        /// <summary>
+        /// Logs a user in using the given StoreAccess
+        /// </summary>
+        /// <param name="storeAccess">The StoreAccess to use</param>
+        /// <returns>True, if the Login was successfull; false if not</returns>
         bool Login(StoreAccess storeAccess);
+
+        /// <summary>
+        /// Logs the current user out
+        /// </summary>
         void Logout();
     }
 }
