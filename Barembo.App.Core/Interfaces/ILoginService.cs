da6cdfe7f3c23b9c79c2a7bca3d5208c9dd0277f
TopperDEL <github_com@tparth.de>
2020-10-27 14:59:17 +0100

    feat: GetLogin from ILoginService

--- Barembo.App.Core/Interfaces/ILoginService.cs
+++ Barembo.App.Core/Interfaces/ILoginService.cs
@@ -19,6 +19,12 @@ namespace Barembo.App.Core.Interfaces
         bool GetIsLoggedIn();
 
         /// <summary>
+        /// Returns the StoreAccess for the current user if logged in
+        /// </summary>
+        /// <returns>The StoreAccess of the user</returns>
+        StoreAccess GetLogin();
+
+        /// <summary>
         /// Logs a user in using the given StoreAccess
         /// </summary>
         /// <param name="storeAccess">The StoreAccess to use</param>
