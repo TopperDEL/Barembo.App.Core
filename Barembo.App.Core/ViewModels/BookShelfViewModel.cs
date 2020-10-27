c0169219a94889a3271bae2b8e1fd8dd4185a34e
TopperDEL <github_com@tparth.de>
2020-10-27 15:00:42 +0100

    feat: Stubb for BookShelfViewModel WIP

---
+++ Barembo.App.Core/ViewModels/BookShelfViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Barembo.Exceptions;
+using Barembo.Interfaces;
+using Barembo.Models;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barembo.App.Core.ViewModels
+{
+    public class BookShelfViewModel : BindableBase
+    {
+        readonly IBookShelfService _bookShelfService;
+
+        private ObservableCollection<BookViewModel> books;
+        public ObservableCollection<BookViewModel> Books
+        {
+            get { return books; }
+            set { SetProperty(ref books, value); }
+        }
+
+        public BookShelfViewModel(IBookShelfService bookShelfService)
+        {
+            Books = new ObservableCollection<BookViewModel>();
+
+            _bookShelfService = bookShelfService;
+        }
+
+        public async Task InitAsync(StoreAccess storeAccess)
+        {
+            BookShelf bookShelf;
+            try
+            {
+                bookShelf = await _bookShelfService.LoadBookShelfAsync(storeAccess);
+            }
+            catch(NoBookShelfExistsException)
+            {
+                //ToDo: Raise event for Navigation to "create Bookshelf" view
+            }
+        }
+    }
+}
---
+++ Barembo.App.Core/ViewModels/BookViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barembo.App.Core.ViewModels
+{
+    public class BookViewModel : BindableBase
+    {
+    }
+}
