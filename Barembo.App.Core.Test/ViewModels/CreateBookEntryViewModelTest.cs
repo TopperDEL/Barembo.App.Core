4afd2cf3e4662832fbdbaf29caf469ff292fa46e
TopperDEL <github_com@tparth.de>
2020-11-04 21:33:32 +0100

    feat: CreateBookEntryViewModel

---
+++ Barembo.App.Core.Test/ViewModels/CreateBookEntryViewModelTest.cs
@@ -0,0 +1,64 @@
+﻿using Barembo.App.Core.Messages;
+using Barembo.App.Core.ViewModels;
+using Barembo.Interfaces;
+using Barembo.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barembo.App.Core.Test.ViewModels
+{
+    [TestClass]
+    public class CreateBookEntryViewModelTest
+    {
+        CreateBookEntryViewModel _viewModel;
+        Moq.Mock<IEntryService> _entryServiceMock;
+        Moq.Mock<IEventAggregator> _eventAggregator;
+        StoreAccess _storeAccess;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _storeAccess = new StoreAccess("use this access");
+            _eventAggregator = new Moq.Mock<IEventAggregator>();
+            _entryServiceMock = new Moq.Mock<IEntryService>();
+            _viewModel = new CreateBookEntryViewModel(_entryServiceMock.Object, _eventAggregator.Object);
+        }
+
+        [TestMethod]
+        public void ExecuteSaveEntry_Publishes_Message()
+        {
+            BookReference bookReference = new BookReference();
+            _eventAggregator.Setup(s => s.GetEvent<BookEntrySavedMessage>()).Returns(new BookEntrySavedMessage()).Verifiable();
+
+            _viewModel.Init(bookReference);
+            _viewModel.SaveEntryCommand.Execute();
+
+            _eventAggregator.Verify();
+        }
+
+        [TestMethod]
+        public void ExecuteSaveEntry_Saves_Entry()
+        {
+            BookReference bookReference = new BookReference();
+            EntryReference entryRef = new EntryReference();
+            Entry entry = new Entry();
+
+            _viewModel.Header = "header";
+            _viewModel.Body = "body";
+
+            _eventAggregator.Setup(s => s.GetEvent<BookEntrySavedMessage>()).Returns(new BookEntrySavedMessage()).Verifiable();
+            _entryServiceMock.Setup(s => s.CreateEntry(_viewModel.Header, _viewModel.Body)).Returns(entry).Verifiable();
+            _entryServiceMock.Setup(s => s.AddEntryToBookAsync(bookReference, entry)).Returns(Task.FromResult(entryRef)).Verifiable();
+
+            _viewModel.Init(bookReference);
+            _viewModel.SaveEntryCommand.Execute();
+
+            _eventAggregator.Verify();
+            _entryServiceMock.Verify();
+        }
+    }
+}
---
+++ Barembo.App.Core/ViewModels/CreateBookEntryViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Barembo.App.Core.Messages;
+using Barembo.Interfaces;
+using Barembo.Models;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barembo.App.Core.ViewModels
+{
+    public class CreateBookEntryViewModel : BindableBase
+    {
+        readonly IEntryService _entryService;
+        readonly IEventAggregator _eventAggregator;
+        private BookReference _bookReference;
+
+        private string _header;
+        public string Header
+        {
+            get { return _header; }
+            set { SetProperty(ref _header, value); }
+        }
+
+        private string _body;
+        public string Body
+        {
+            get { return _body; }
+            set { SetProperty(ref _body, value); }
+        }
+
+        private DelegateCommand _saveEntryCommand;
+        public DelegateCommand SaveEntryCommand =>
+            _saveEntryCommand ?? (_saveEntryCommand = new DelegateCommand(async () => await ExecuteSaveEntryCommand().ConfigureAwait(false), CanExecuteSaveEntryCommand));
+
+        async Task ExecuteSaveEntryCommand()
+        {
+            Entry entry = _entryService.CreateEntry(Header, Body);
+
+            var entryReference = await _entryService.AddEntryToBookAsync(_bookReference, entry);
+
+            _eventAggregator.GetEvent<BookEntrySavedMessage>().Publish(new Tuple<BookReference, EntryReference>(_bookReference, entryReference));
+        }
+
+        bool CanExecuteSaveEntryCommand()
+        {
+            return true;
+        }
+
+        public CreateBookEntryViewModel(IEntryService entryService, IEventAggregator eventAggregator)
+        {
+            _entryService = entryService;
+            _eventAggregator = eventAggregator;
+        }
+
+        public void Init(BookReference bookReference)
+        {
+            _bookReference = bookReference;
+        }
+    }
+}
