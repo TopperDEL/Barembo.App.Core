784f0ee382add11e7c1d086deba9b27e04b42eb1
TopperDEL <github_com@tparth.de>
2020-11-08 20:09:53 +0100

    feat: EntryViewModel & BookEntriesViewModel WIP

---
+++ Barembo.App.Core/ViewModels/BookEntriesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Barembo.App.Core.ViewModels
+{
+    public class BookEntriesViewModel : BindableBase
+    {
+        private ObservableCollection<EntryViewModel> _entries;
+        public ObservableCollection<EntryViewModel> Entries
+        {
+            get { return _entries; }
+            set { SetProperty(ref _entries, value); }
+        }
+
+        public BookEntriesViewModel()
+        {
+
+        }
+
+        public void InitEntries(ObservableCollection<EntryViewModel> entries)
+        {
+            _entries = entries;
+        }
+    }
+}
---
+++ Barembo.App.Core/ViewModels/EntryViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Barembo.Interfaces;
+using Barembo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barembo.App.Core.ViewModels
+{
+    public class EntryViewModel : AsyncLoadingBindableBase
+    {
+        private readonly EntryReference _entryReference;
+        private readonly IEntryService _entryService;
+        private Entry _entry;
+
+        public string Header
+        {
+            get
+            {
+                if (_entry == null)
+                {
+                    LoadEntry();
+                    return "...";
+                }
+                return _entry.Header;
+            }
+        }
+
+        public string Body
+        {
+            get
+            {
+                if (_entry == null)
+                {
+                    LoadEntry();
+                    return "...";
+                }
+                return _entry.Body;
+            }
+        }
+
+        public byte[] Thumbnail
+        {
+            get
+            {
+                if (_entry == null)
+                {
+                    LoadEntry();
+                    return null; //ToDo: Return placeholder-Image
+                }
+                else if (string.IsNullOrEmpty(_entry.ThumbnailBase64))
+                    return null;
+
+                return Convert.FromBase64String(_entry.ThumbnailBase64);
+            }
+        }
+
+        public EntryViewModel(EntryReference entryReference, IEntryService entryService)
+        {
+            _entryReference = entryReference;
+            _entryService = entryService;
+        }
+
+        private void LoadEntry()
+        {
+            if (IsLoading)
+                return;
+            
+            IsLoading = true;
+
+            _entryService.LoadEntryAsSoonAsPossible(_entryReference, (loadedEntry) => InitFromEntry(loadedEntry), () => IsLoading = false);
+        }
+
+        private void InitFromEntry(Entry entry)
+        {
+            _entry = entry;
+
+            IsLoading = false;
+
+            RaisePropertyChanged(nameof(Header));
+            RaisePropertyChanged(nameof(Body));
+            RaisePropertyChanged(nameof(Thumbnail));
+        }
+    }
+}
