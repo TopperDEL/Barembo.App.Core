c9013b20641c7c7c0cbfb01d254f48d762f9e3f7
TopperDEL <github_com@tparth.de>
2020-12-27 20:03:37 +0100

    feat: IMEdiaFetchService + Nuget v0.9.28

---
+++ Barembo.App.Core/Interfaces/IMediaFetchService.cs
@@ -0,0 +1,14 @@
+﻿using Barembo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barembo.App.Core.Interfaces
+{
+    public interface IMediaFetchService
+    {
+        Task<Tuple<Attachment, Stream>> FetchMediaAsync();
+    }
+}
