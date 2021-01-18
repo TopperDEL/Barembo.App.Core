0e78c4f95f57ccb8986fd4ab374fda536b4f50d2
TopperDEL <github_com@tparth.de>
2021-01-18 21:09:41 +0100

    fix: Tests

--- Barembo.App.Core.Test/ViewModels/ShareBookViewModelTest.cs
+++ Barembo.App.Core.Test/ViewModels/ShareBookViewModelTest.cs
@@ -29,24 +29,7 @@ namespace Barembo.App.Core.Test.ViewModels
         }
 
         [TestMethod]
-        public void ExecuteSaveBookShare_Saves_BookSharePublishes_Message()
-        {
-            //BookReference bookReference = new BookReference();
-            //StoreAccess storeAccess = new StoreAccess();
-            //BookShareReference bookShareReference = new BookShareReference();
-            //_viewModel.ContributorName = "Tim";
-            //_viewModel.AccessRights.CanAddEntries = true;
-
-            //_bookShelfServiceMock.Setup(s => s.ShareBookAsync(storeAccess, bookReference, _viewModel.ContributorName, _viewModel.AccessRights)).Returns(Task.FromResult(bookShareReference)).Verifiable();
-
-            //_viewModel.Init(storeAccess, bookReference);
-            //_viewModel.SaveBookShareCommand.Execute();
-
-            //_bookShelfServiceMock.Verify();
-        }
-
-        [TestMethod]
-        public void ExecuteSaveBookShare_Publishes_Message()
+        public void ExecuteSaveBookShare_SavesAndPublishesMessage()
         {
             BookReference bookReference = new BookReference();
             StoreAccess storeAccess = new StoreAccess();
