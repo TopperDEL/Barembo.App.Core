0f2ec09768e8be6fbb1300ba68d6d93adcb13519
TopperDEL <github_com@tparth.de>
2021-01-20 20:52:14 +0100

    feat: ShowBookShareViewModel + Tests

---
+++ Barembo.App.Core.Test/ViewModels/ShowBookShareViewModelTest.cs
@@ -0,0 +1,70 @@
+﻿using Barembo.App.Core.Messages;
+using Barembo.App.Core.ViewModels;
+using Barembo.Interfaces;
+using Barembo.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barembo.App.Core.Test.ViewModels
+{
+    [TestClass]
+    public class ShowBookShareViewModelTest
+    {
+        ShowBookShareViewModel _viewModel;
+        Moq.Mock<IEventAggregator> _eventAggregatorMock;
+        Moq.Mock<IQRCodeGeneratorService> _qrCodeGeneratorService;
+        Moq.Mock<IMagicLinkGeneratorService> _magicLinkGeneratorService;
+        BookShareReference _bookShareReference;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _bookShareReference = new BookShareReference();
+            _eventAggregatorMock = new Moq.Mock<IEventAggregator>();
+            _qrCodeGeneratorService = new Moq.Mock<IQRCodeGeneratorService>();
+            _magicLinkGeneratorService = new Moq.Mock<IMagicLinkGeneratorService>();
+            _viewModel = new ShowBookShareViewModel(_eventAggregatorMock.Object, _qrCodeGeneratorService.Object, _magicLinkGeneratorService.Object);
+        }
+
+        [TestMethod]
+        public void Init_Generates_QRCodeAndMagicLink()
+        {
+            byte[] qrCodePNG = new byte[10];
+
+            _qrCodeGeneratorService.Setup(s => s.GetQRCodePNGFor(_bookShareReference)).Returns(qrCodePNG).Verifiable();
+            _magicLinkGeneratorService.Setup(s => s.GetMagicLinkFor(_bookShareReference)).Returns("myMagicLink").Verifiable();
+
+            _viewModel.Init(_bookShareReference);
+
+            Assert.AreEqual(qrCodePNG, _viewModel.QRCodePNG);
+            Assert.AreEqual("myMagicLink", _viewModel.MagicLink);
+
+            _qrCodeGeneratorService.Verify();
+            _magicLinkGeneratorService.Verify();
+        }
+
+        [TestMethod]
+        public void ExecuteWriteMagicLinkToClipboard_Raises_WriteToClipboardMessage()
+        {
+            _viewModel.MagicLink = "thisShouldBeWritten";
+
+            _eventAggregatorMock.Setup(s => s.GetEvent<WriteToClipboardMessage>()).Returns(new WriteToClipboardMessage()).Verifiable();
+
+            _viewModel.WriteMagicLinkToClipboardCommand.Execute();
+
+            _eventAggregatorMock.Verify();
+        }
+
+        [TestMethod]
+        public void GoBack_Goes_Back()
+        {
+            _eventAggregatorMock.Setup(s => s.GetEvent<GoBackMessage>()).Returns(new GoBackMessage()).Verifiable();
+            _viewModel.GoBackCommand.Execute();
+
+            _eventAggregatorMock.Verify();
+        }
+    }
+}
---
+++ Barembo.App.Core/ViewModels/ShowBookShareViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Barembo.App.Core.Messages;
+using Barembo.Interfaces;
+using Barembo.Models;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barembo.App.Core.ViewModels
+{
+    public class ShowBookShareViewModel : BindableBase
+    {
+        readonly IEventAggregator _eventAggregator;
+        readonly IQRCodeGeneratorService _qrCodeGeneratorService;
+        readonly IMagicLinkGeneratorService _magicLinkGeneratorService;
+
+        private string _magicLink;
+        public string MagicLink
+        {
+            get { return _magicLink; }
+            set { SetProperty(ref _magicLink, value); }
+        }
+
+        private byte[] _qrCodePNG;
+        public byte[] QRCodePNG
+        {
+            get { return _qrCodePNG; }
+            set { SetProperty(ref _qrCodePNG, value); }
+        }
+
+        private DelegateCommand _writeMagicLinkToClipboardCommand;
+        public DelegateCommand WriteMagicLinkToClipboardCommand =>
+            _writeMagicLinkToClipboardCommand ?? (_writeMagicLinkToClipboardCommand = new DelegateCommand(ExecuteWriteToClipboardCommand));
+
+        private DelegateCommand _goBackCommand;
+        public DelegateCommand GoBackCommand =>
+            _goBackCommand ?? (_goBackCommand = new DelegateCommand(ExecuteGoBackCommand));
+
+        void ExecuteGoBackCommand()
+        {
+            _eventAggregator.GetEvent<GoBackMessage>().Publish();
+        }
+
+        void ExecuteWriteToClipboardCommand()
+        {
+            _eventAggregator.GetEvent<WriteToClipboardMessage>().Publish(MagicLink);
+        }
+
+        public ShowBookShareViewModel(IEventAggregator eventAggregator, IQRCodeGeneratorService qrCodeGeneratorService, IMagicLinkGeneratorService magicLinkGeneratorService)
+        {
+            _eventAggregator = eventAggregator;
+            _qrCodeGeneratorService = qrCodeGeneratorService;
+            _magicLinkGeneratorService = magicLinkGeneratorService;
+        }
+
+        public void Init(BookShareReference bookShareReference)
+        {
+            QRCodePNG = _qrCodeGeneratorService.GetQRCodePNGFor(bookShareReference);
+            MagicLink = _magicLinkGeneratorService.GetMagicLinkFor(bookShareReference);
+        }
+    }
+}
